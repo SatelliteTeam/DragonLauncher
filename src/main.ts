f1deb0cf7782356d5114126c31756215ebbae958
D4RKAR117 <D4RKAR117@outlook.es>
2020-08-22 16:54:01 -0500

    Added base styles and configuration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(fas, fab, far);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+import '@/assets/sass/app.scss';
 
 Vue.config.productionTip = false;
 
