bb6e6795de5efdb67019e6099fd4b548489f6fcc
D4RKAR117 <D4RKAR117@outlook.es>
2020-08-22 20:37:15 -0500

    Added Loader logo animation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,20 @@
 <template>
-	<div id="login"></div>
+	<b-container id="login" fluid>
+		<b-row align-v="stretch" align-content="center" align-h="center">
+			<b-col id="logo-wrapper" cols="8">
+				<logo :direction="'alternate'" :duration="5500"></logo>
+			</b-col>
+			<b-col id="form-wrapper" cols="4"></b-col>
+		</b-row>
+	</b-container>
 </template>
 <script lang="ts">
 import Vue from 'vue';
+import Logo from '@/components/SvgLogo.vue';
 export default Vue.extend({
 	name: 'Login',
+	components: {
+		Logo,
+	},
 });
 </script>
